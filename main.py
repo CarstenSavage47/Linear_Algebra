6dc011a8675a993b567af664c1dc902677cfde5f
CarstenSavage47 <rampagev2@gmail.com>
2023-02-02 20:38:35 -0800

    Adding stuff

--- main.py
+++ main.py
@@ -9,5 +9,17 @@ for a, b in zip(vector, vector_t):
     print(f"{a}+{b}: ",a+b)
     dot_product_list.append(a+b)
 
-sum(dot_product_list)
+print(sum(dot_product_list))
+
+# 2-norm
+# Square root of sums of the squares of entries in vector
+
+square_of_entry = []
+
+for a in vector:
+    print(f"{a}^2:",a**2)
+    square_of_entry.append(a**2)
+
+print(sum(square_of_entry))
+
 
