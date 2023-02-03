aa64e3c4ad217b79b35294ba06596db7f62afddf
CarstenSavage47 <rampagev2@gmail.com>
2023-02-02 21:30:00 -0800

    Adding stuff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-'''Example 1: The dot product of a vector with itself is the two-norm of that vector, squared.'''
+'''Version 1: The dot product of a vector with itself is the two-norm of that vector, squared.'''
 
 vector = np.array([3, 4])
 
@@ -25,3 +25,14 @@ for a in vector:
 print(sum(square_of_entry))
 
 
+'''Version 2: The dot product of a vector with itself is the two-norm of that vector, squared.'''
+
+u = np.array([3, 4])
+
+dot_product = np.dot(u, u)
+
+# Calculate the two-norm of u using the np.linalg.norm function with ord=2
+two_norm = np.linalg.norm(u, ord=2)
+
+print(two_norm)
+print(two_norm**2)
