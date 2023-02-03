c126c0e35d8ebc1087b3db79a4d3106feb41aa8f
CarstenSavage47 <rampagev2@gmail.com>
2023-02-02 20:50:53 -0800

    Adding stuff

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-vector = np.array([1, 2, 3, 4])
+vector = np.array([3, 4])
 
 vector_t = vector.transpose()
 
 dot_product_list = []
 for a, b in zip(vector, vector_t):
-    print(f"{a}+{b}: ",a+b)
-    dot_product_list.append(a+b)
+    print(f"{a}*{b}: ",a*b)
+    dot_product_list.append(a*b)
 
 print(sum(dot_product_list))
 
